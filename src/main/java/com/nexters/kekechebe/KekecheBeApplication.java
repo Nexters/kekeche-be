30a60dc2248303062939e2f97271469ddbd1c63f
eunsol-an <dev.eunsol@gmail.com>
2024-01-21 22:15:55 +0900

    [CHORE] 프로젝트 초기 세팅

---
+++ src/main/java/com/nexters/kekechebe/KekecheBeApplication.java
@@ -0,0 +1,13 @@
+package com.nexters.kekechebe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KekecheBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KekecheBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nexters/kekechebe/KekecheBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nexters.kekechebe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KekecheBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

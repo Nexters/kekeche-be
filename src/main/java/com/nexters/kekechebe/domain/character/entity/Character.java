827ad3760d6b28b79a43c58c05a23f854edff4ab
맹순영 <symaeng98@naver.com>
2024-01-23 02:15:05 +0900

    [FEAT] 엔티티 생성

--- src/main/java/com/nexters/kekechebe/domain/character/entity/Character.java
+++ src/main/java/com/nexters/kekechebe/domain/character/entity/Character.java
@@ -1,5 +1,6 @@
 package com.nexters.kekechebe.domain.character.entity;
 
+import com.nexters.kekechebe.domain.member.entity.Member;
 import com.nexters.kekechebe.domain.memo.entity.Memo;
 import com.nexters.kekechebe.util.Timestamped;
 import jakarta.persistence.*;
@@ -47,6 +48,10 @@ public class Character extends Timestamped {
 
     @Column(name = "keyword")
     private String keyword;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
 
     @OneToMany(mappedBy = "character", fetch = FetchType.LAZY, cascade = CascadeType.REMOVE)
     private List<Memo> memos = new ArrayList<>();

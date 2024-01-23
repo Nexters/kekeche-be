a64fb5dfcf098bfc4e4e6166ca6e64bb1460f822
맹순영 <symaeng98@naver.com>
2024-01-23 16:12:11 +0900

    [FIX] 응답 방식 수정 (ResponseEntity -> BaseResponse)

--- src/main/java/com/nexters/kekechebe/domain/memo/controller/MemoController.java
+++ src/main/java/com/nexters/kekechebe/domain/memo/controller/MemoController.java
@@ -7,8 +7,6 @@ import com.nexters.kekechebe.dto.BaseResponse;
 import com.nexters.kekechebe.exceptions.StatusCode;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -18,21 +16,21 @@ public class MemoController {
     private final MemoService memoService;
 
     @PostMapping()
-    public ResponseEntity<BaseResponse> saveMemo(@RequestBody MemoCreateRequest request, HttpServletRequest httpServletRequest) {
+    public BaseResponse saveMemo(@RequestBody MemoCreateRequest request, HttpServletRequest httpServletRequest) {
 //        long memberId = Long.parseLong(httpServletRequest.getAttribute("memberId").toString()); 회원 구현에 따라 변경 예정
         long memberId = 1L;
 
         memoService.saveMemo(memberId, request);
-        return ResponseEntity.status(HttpStatus.CREATED).body(new BaseResponse(StatusCode.OK));
+        return new BaseResponse(StatusCode.CREATED);
     }
 
     @PutMapping("/{memoId}")
-    public ResponseEntity<BaseResponse> updateMemo(@PathVariable("memoId") long memoId, @RequestBody MemoUpdateRequest request, HttpServletRequest httpServletRequest) {
+    public BaseResponse updateMemo(@PathVariable("memoId") long memoId, @RequestBody MemoUpdateRequest request, HttpServletRequest httpServletRequest) {
 //        long memberId = Long.parseLong(httpServletRequest.getAttribute("memberId").toString()); 회원 구현에 따라 변경 예정
         long memberId = 1L;
         memoService.updateMemo(memberId, memoId, request);
 
-        return ResponseEntity.status(HttpStatus.OK).body(new BaseResponse(StatusCode.OK));
+        return new BaseResponse(StatusCode.OK);
     }
 
     @DeleteMapping("/{memoId}")

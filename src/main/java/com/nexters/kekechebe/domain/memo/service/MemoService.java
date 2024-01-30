c6d46f96a33f69c785c111795810f110f38c3315
맹순영 <symaeng98@naver.com>
2024-01-30 21:34:38 +0900

    [FIX] 회원 조회 방식 수정

--- src/main/java/com/nexters/kekechebe/domain/memo/service/MemoService.java
+++ src/main/java/com/nexters/kekechebe/domain/memo/service/MemoService.java
@@ -29,12 +29,10 @@ public class MemoService {
     private final CharacterRepository characterRepository;
 
     @Transactional
-    public void saveMemo(long memberId, MemoCreateRequest request) {
+    public void saveMemo(Member member, MemoCreateRequest request) {
         long characterId = request.getCharacterId();
         String content = request.getContent();
 
-        Member member = memberRepository.findById(memberId)
-                .orElseThrow(() -> new NoResultException("회원을 찾을 수 없습니다."));
         Character character = characterRepository.findById(characterId)
                 .orElseThrow(() -> new NoResultException("캐릭터를 찾을 수 없습니다."));
 
@@ -49,18 +47,13 @@ public class MemoService {
         memoRepository.save(memo);
     }
 
-    public MemoPage getAllMemos(long memberId, Pageable pageable) {
-        Member member = memberRepository.findById(memberId)
-                .orElseThrow(() -> new NoResultException("회원을 찾을 수 없습니다."));
-
+    public MemoPage getAllMemos(Member member, Pageable pageable) {
         Page<Memo> memos = memoRepository.findAllByMember(member, pageable);
 
         return MemoPage.from(memos);
     }
 
-    public MemoPage getCharacterMemos(long memberId, long characterId, Pageable pageable) {
-        Member member = memberRepository.findById(memberId)
-                .orElseThrow(() -> new NoResultException("회원을 찾을 수 없습니다."));
+    public MemoPage getCharacterMemos(Member member, long characterId, Pageable pageable) {
         Character character = characterRepository.findById(characterId)
                 .orElseThrow(() -> new NoResultException("캐릭터를 찾을 수 없습니다."));
 
@@ -70,9 +63,7 @@ public class MemoService {
     }
 
     @Transactional
-    public void updateMemo(long memberId, long memoId, MemoUpdateRequest request) {
-        Member member = memberRepository.findById(memberId)
-                .orElseThrow(() -> new NoResultException("회원을 찾을 수 없습니다."));
+    public void updateMemo(Member member, long memoId, MemoUpdateRequest request) {
         Memo memo = memoRepository.findByIdAndMember(memoId, member)
                 .orElseThrow(() -> new NoResultException("기록을 찾을 수 없습니다."));
 
@@ -80,9 +71,7 @@ public class MemoService {
     }
 
     @Transactional
-    public void deleteMemo(long memberId, long memoId) {
-        Member member = memberRepository.findById(memberId)
-                .orElseThrow(() -> new NoResultException("회원을 찾을 수 없습니다."));
+    public void deleteMemo(Member member, long memoId) {
         Memo memo = memoRepository.findByIdAndMember(memoId, member)
                 .orElseThrow(() -> new NoResultException("기록을 찾을 수 없습니다."));
 

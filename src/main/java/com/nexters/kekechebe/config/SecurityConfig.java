ee84b50ae7c99140ca45e3b3bdc43584fabe79d7
맹순영 <symaeng98@naver.com>
2024-02-03 03:19:29 +0900

    [FIX] 캐릭터 생성 후 로그인 security config 추가

--- src/main/java/com/nexters/kekechebe/config/SecurityConfig.java
+++ src/main/java/com/nexters/kekechebe/config/SecurityConfig.java
@@ -49,7 +49,7 @@ public class SecurityConfig {
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 
             .authorizeHttpRequests((authorize) -> authorize
-                .requestMatchers("/api/v1/auth/kakao/callback", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                .requestMatchers("/api/v1/auth/kakao/callback", "/api/v1/auth/character-kakao/callback", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
                 .requestMatchers(HttpMethod.GET, "/api/v1/character/member/**").permitAll()
                 .anyRequest().authenticated()
             )

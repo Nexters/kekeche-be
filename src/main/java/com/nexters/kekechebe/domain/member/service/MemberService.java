575dec060425d04cc9e23abe94325ed1470e5969
eunsol-an <dev.eunsol@gmail.com>
2024-02-12 00:02:54 +0900

    [FEAT] 응원하기 기능 구현

--- src/main/java/com/nexters/kekechebe/domain/member/service/MemberService.java
+++ src/main/java/com/nexters/kekechebe/domain/member/service/MemberService.java
@@ -4,10 +4,13 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.nexters.kekechebe.domain.character.repository.CharacterRepository;
+import com.nexters.kekechebe.domain.member.dto.response.MemberCheerResponse;
 import com.nexters.kekechebe.domain.member.dto.response.MemberResponse;
 import com.nexters.kekechebe.domain.member.entity.Member;
+import com.nexters.kekechebe.domain.member.repository.MemberRepository;
 import com.nexters.kekechebe.domain.memo.repository.MemoRepository;
 
+import jakarta.persistence.NoResultException;
 import lombok.RequiredArgsConstructor;
 
 @Service
@@ -16,6 +19,7 @@ import lombok.RequiredArgsConstructor;
 public class MemberService {
     private final CharacterRepository characterRepository;
     private final MemoRepository memoRepository;
+    private final MemberRepository memberRepository;
 
     public MemberResponse getMemberInfo(Member member) {
         long characterCount = characterRepository.countCharacterByMember(member);
@@ -26,6 +30,17 @@ public class MemberService {
             .nickname(member.getNickname())
             .characterCount(characterCount)
             .memoCount(memoCount)
+            .build();
+    }
+
+    @Transactional
+    public MemberCheerResponse updateCheerCount(Long accessMemberId) {
+        Member accessMember = memberRepository.findById(accessMemberId)
+            .orElseThrow(() -> new NoResultException("회원을 찾을 수 없습니다."));
+        Integer nextCount = accessMember.updateCheerCount(1);
+        return MemberCheerResponse.builder()
+            .memberId(accessMember.getId())
+            .cheerCount(nextCount)
             .build();
     }
 }

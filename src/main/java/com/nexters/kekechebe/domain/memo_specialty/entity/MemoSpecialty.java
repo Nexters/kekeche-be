734f7891d0e15afd463ff8ce16cf55f7dd5929ac
맹순영 <symaeng98@naver.com>
2024-02-12 17:31:29 +0900

    [FEAT] 메모 주특기 엔티티 생성

--- src/main/java/com/nexters/kekechebe/domain/memo_specialty/entity/MemoSpecialty.java
+++ src/main/java/com/nexters/kekechebe/domain/memo_specialty/entity/MemoSpecialty.java
@@ -1,6 +1,7 @@
-package com.nexters.kekechebe.domain.hashtag.entity;
+package com.nexters.kekechebe.domain.memo_specialty.entity;
 
 import com.nexters.kekechebe.domain.memo.entity.Memo;
+import com.nexters.kekechebe.domain.specialty.entity.Specialty;
 import com.nexters.kekechebe.util.Timestamped;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
@@ -13,27 +14,28 @@ import org.hibernate.annotations.SQLRestriction;
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@Table(name = "hashtag")
+@Table(name = "memo_specialty")
 @SQLRestriction("deleted_at IS NULL")
-@SQLDelete(sql = "UPDATE hashtag SET deleted_at = current_timestamp WHERE id = ?")
-public class Hashtag extends Timestamped {
+@SQLDelete(sql = "UPDATE memo_specialty SET deleted_at = current_timestamp WHERE id = ?")
+public class MemoSpecialty extends Timestamped {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "content", nullable = false)
-    private String content;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "specialty_id", nullable = false)
+    private Specialty specialty;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "memo_id", nullable = false)
     private Memo memo;
 
     @Builder
-    public Hashtag(
-            String content,
+    public MemoSpecialty(
+            Specialty specialty,
             Memo memo
     ) {
-        this.content = content;
+        this.specialty = specialty;
         this.memo = memo;
     }
 }
---
+++ src/main/java/com/nexters/kekechebe/domain/memo_specialty/repository/MemoSpecialtyRepository.java
@@ -0,0 +1,7 @@
+package com.nexters.kekechebe.domain.memo_specialty.repository;
+
+import com.nexters.kekechebe.domain.memo_specialty.entity.MemoSpecialty;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemoSpecialtyRepository extends JpaRepository<MemoSpecialty, Long> {
+}

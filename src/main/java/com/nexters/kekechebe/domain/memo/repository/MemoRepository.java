9e21fd52296427b336288c94710177f49900a97b
맹순영 <symaeng98@naver.com>
2024-02-13 23:41:19 +0900

    [FIX] @Param 추가

--- src/main/java/com/nexters/kekechebe/domain/memo/repository/MemoRepository.java
+++ src/main/java/com/nexters/kekechebe/domain/memo/repository/MemoRepository.java
@@ -7,23 +7,24 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDateTime;
 import java.util.Optional;
 
 public interface MemoRepository extends JpaRepository<Memo, Long> {
     @Query("SELECT m FROM Memo m JOIN FETCH m.character JOIN FETCH m.memoSpecialties WHERE m.id = :memoId AND m.member = :member")
-    Optional<Memo> findByIdAndMember(Long memoId, Member member);
+    Optional<Memo> findByIdAndMember(@Param("memoId") Long memoId, @Param("member") Member member);
 
     @Query("SELECT m FROM Memo m JOIN FETCH m.character WHERE m.member = :member")
-    Page<Memo> findAllByMember(Member member, Pageable pageable);
+    Page<Memo> findAllByMember(@Param("member") Member member, Pageable pageable);
 
     Page<Memo> findAllByMemberAndCharacter(Member member, Character character, Pageable pageable);
 
     int countByMemberAndCharacterAndCreatedAtBetween(Member member, Character character, LocalDateTime startDateTime, LocalDateTime endDateTime);
 
     @Query("SELECT m FROM Memo m JOIN FETCH m.character WHERE m.member = :member and m.content LIKE %:keyword%")
-    Page<Memo> searchAllByKeyword(Member member, String keyword, Pageable pageable);
+    Page<Memo> searchAllByKeyword(@Param("member") Member member, @Param("keyword") String keyword, Pageable pageable);
 
     long countMemoByMember(Member member);
 }
